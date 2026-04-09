9449fbaffcacdee230d1f21998a6ea51b6f1f899
Martin Persson <martin@persson.engineer>
2026-04-09 13:44:11 +0200

    Fix infinite ammo: use sv_infinite_ammo 1 via ExecuteCommand with sv_cheats

--- RandomRoundEvents.cs
+++ RandomRoundEvents.cs
@@ -262,8 +262,7 @@ public class RandomRoundEvents : BasePlugin, IPluginConfig<RandomRoundEventsConf
                 AnnounceEvent("Juan Deag Round", "Deagle only, headshots only. One tap or nothing!");
                 RegisterEventHandler<EventPlayerHurt>(OnPlayerHurt, HookMode.Post); _hurtHandlerRegistered = true;
                 StripAllWeapons(); GiveAllPlayersKnives(); GiveAllPlayersDeagle();
-                var infiniteAmmo = ConVar.Find("sv_infinite_ammo");
-                if (infiniteAmmo != null) infiniteAmmo.SetValue(2);
+                Server.ExecuteCommand("sv_infinite_ammo 1");
                 break;
             case EventType.RandomWeapon:
                 AnnounceEvent("Random Weapon Round", "Everyone gets a random weapon. Good luck!");
@@ -591,9 +590,7 @@ public class RandomRoundEvents : BasePlugin, IPluginConfig<RandomRoundEventsConf
         ResetAllPlayersVisibility();
         Server.ExecuteCommand("mp_respawn_on_death_t 0; mp_respawn_on_death_ct 0; mp_randomspawn 0; mp_buytime 20");
         EnableBuying();
-        Server.ExecuteCommand("mp_taser_recharge_time 30");
-        var infiniteAmmo = ConVar.Find("sv_infinite_ammo");
-        if (infiniteAmmo != null) infiniteAmmo.SetValue(0);
+        Server.ExecuteCommand("mp_taser_recharge_time 30; sv_infinite_ammo 0");
         Server.ExecuteCommand("mp_death_drop_gun 0");
         ResetMaxHealth();
         _activeEvent = EventType.None;
